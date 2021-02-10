73c36de0a9721b4c58521f48b080ddd007ed41b1
mlmaxi98 <cardozojoaquinm@gmail.com>
2021-02-09 23:10:02 -0300

    09-02-21

---
+++ src/Heroes/app.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var fs = require('fs');
+
+const agilidad = [{
+        name: 'Nevermore',
+        title: 'Shadow Fiend',
+        skills: [],
+        profilePic: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/nevermore_full.png?v=6358140?v=6358140'
+    },
+    {
+        name: 'Mortred',
+        title: 'Phantom Assasin',
+        skills: [],
+        profilePic: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/phantom_assassin_full.png?v=6358140?v=6358140'
+    },
+    {
+        name: 'Mercurial',
+        title: 'Spectre',
+        skills: [],
+        profilePic: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/spectre_full.png?v=6358140?v=6358140'
+    },
+    {
+        name: 'Nightcrawler',
+        title: 'Slark',
+        skills: [],
+        profilePic: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/slark_full.png?v=6358140?v=6358140'
+    }
+]
+export default agilidad
--- src/components/Heroes.jsx
+++ src/components/Heroes.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 //import {NavLink} from 'react-router-dom'
 import '../styled/Heroes.css'
-
+import agilidad from '../Heroes/app'
 function Heroes(){
     return(
         <div className ='container'>
-
-            <div className ='body'>
+            {/* <div className ='body'>
                 <img src= 'https://i.redd.it/8le3puzyk45y.png' alt='' className='fondo'></img>
-            </div>
+            </div> */}
+            <ul className = 'heroes-a'>
+                {agilidad.map(heroe =><li>
 
+                <img src = {heroe.profilePic}></img>
+                </li>)
+                }
+            </ul>
         </div>
     )
 }
